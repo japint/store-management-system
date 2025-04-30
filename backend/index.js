025e490383bc5c7a64e9ba7663330bd6797face6
meistergpc <jap1centeno@gmail.com>
2025-04-30 15:33:17 +0200

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -1,63 +1,27 @@
+require("dotenv").config();
+
 // instantiation
 const express = require("express");
-const mysql = require("mysql");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const contextMiddleware = require("./middlewares/contextMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
-
-const cors = require("cors");
-
-// import store/user/LogStore
-const UserStore = require("./src/users/store");
-const ItemStore = require("./src/items/store");
-const LogStore = require("./src/logs/store");
-const UserService = require("./src/users/service");
-const ItemService = require("./src/items/service");
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(contextMiddleware);
 
-// databse connection
-app.use((req, res, next) => {
-  const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Eyzio!t!t6*ZdaLo",
-    database: "mydb",
-  });
-  connection.connect();
-
-  const userStore = new UserStore({ db: connection });
-  const itemStore = new ItemStore({ db: connection });
-  const logStore = new LogStore({ db: connection });
-  const userService = new UserService({ userStore, logStore });
-  const itemService = new ItemService({ itemStore, logStore });
-
-  // app.use middleware
-  req.context = {
-    userStore,
-    itemStore,
-    logStore,
-    userService,
-    itemService,
-  };
-
-  res.on("finish", () => {
-    connection.end();
-  });
-  next();
-});
-
-// protect middleware
+// JWT protect middleware
 const privateKey = "qwerty";
 
 const protect = (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    const token = authHeader && authHeader.split(" ")[1]; // check
+    const token = authHeader && authHeader.split(" ")[1];
     if (!token) return res.status(401).send("No token provided");
 
     const verify = jwt.verify(token, privateKey);
@@ -73,7 +37,6 @@ const protect = (req, res, next) => {
 
 // routing | register the route
 app.use("/user", require("./src/users/route"));
-
 app.use("/item", protect, require("./src/items/route"));
 app.use("/log", protect, require("./src/logs/route"));
 app.use(require("./src/auth/route"));
---
+++ backend/middlewares/contextMiddleware.js
@@ -0,0 +1,50 @@
+require("dotenv").config();
+
+const mysql = require("mysql");
+// import store/user/LogStore
+const UserStore = require("../src/users/store");
+const ItemStore = require("../src/items/store");
+const LogStore = require("../src/logs/store");
+const UserService = require("../src/users/service");
+const ItemService = require("../src/items/service");
+
+// databse connection
+const contextMiddleware = (req, res, next) => {
+  const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("❌ DB connection failed:");
+      console.error(err.code); // Like ER_ACCESS_DENIED_ERROR
+      console.error(err.sqlMessage);
+      return res.status(500).send("DB connection error");
+    }
+
+    const userStore = new UserStore({ db: connection });
+    const itemStore = new ItemStore({ db: connection });
+    const logStore = new LogStore({ db: connection });
+    const userService = new UserService({ userStore, logStore });
+    const itemService = new ItemService({ itemStore, logStore });
+
+    req.context = {
+      userStore,
+      itemStore,
+      logStore,
+      userService,
+      itemService,
+    };
+
+    res.on("finish", () => {
+      connection.end();
+    });
+
+    next();
+  });
+};
+
+module.exports = contextMiddleware;
