d8c5187d5f325b5b36c804a3cff1bfd8e52e8cd0
meistergpc <jap1centeno@gmail.com>
2025-04-30 20:14:38 +0200

    Ready to view  github pages

--- backend/src/users/store.js
+++ backend/src/users/store.js
@@ -97,7 +97,7 @@ class UserStore {
     return await this.get(id);
   }
 
-  // update v1
+  // update
   async update(uid, user) {
     const { name, username, pw } = user;
 
@@ -120,54 +120,6 @@ class UserStore {
 
     return { ...user, uid: uid };
   }
-
-  // update v2
-  // async update(uid, user) {
-  //   console.log({ uid, user });
-  //   const { name, username, pw } = user;
-
-  //   return new Promise((resolve, reject) => {
-  //     const values = [];
-  //     let query = "UPDATE users SET";
-  //     const fields = [];
-
-  //     // Always update name
-  //     fields.push(" name = ?");
-  //     values.push(name);
-
-  //     // Conditionally update username
-  //     if (username) {
-  //       fields.push(" username = ?");
-  //       values.push(username);
-  //     }
-
-  //     // Conditionally update password
-  //     const finalize = (finalValues) => {
-  //       query += fields.join(",");
-  //       query += " WHERE uid = ?";
-  //       finalValues.push(uid);
-
-  //       this.db.query(query, finalValues, (err, rows, fields) => {
-  //         if (err) return reject(err);
-  //         resolve({ ...user, itemid: uid });
-  //       });
-  //     };
-
-  //     // If password update is needed, hash it first
-  //     if (pw) {
-  //       const bcrypt = require("bcrypt");
-  //       const saltRounds = 10;
-  //       bcrypt.hash(pw, saltRounds, (err, hashedPassword) => {
-  //         if (err) return reject(err);
-  //         fields.push(" pw = ?");
-  //         values.push(hashedPassword);
-  //         finalize(values);
-  //       });
-  //     } else {
-  //       finalize(values);
-  //     }
-  //   });
-  // }
 
   // delete
   async delete(id) {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,20 +2,54 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  const [form, setForm] = useState({ name: "", pw: "", username: "" });
+  const [registerForm, setRegisterForm] = useState({
+    name: "",
+    pw: "",
+    username: "",
+  });
+  const [loginForm, setLoginForm] = useState({ pw: "", username: "" });
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
+  const registerChange = (e) => {
+    setRegisterForm({ ...registerForm, [e.target.name]: e.target.value });
+  };
+  const loginChange = (e) => {
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e) => {
+  const handleRegister = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    setMessage("");
+
+    await new Promise((res) => setTimeout(res, 3000));
+
     try {
-      const res = await axios.post("http://localhost:4000/user/register", form);
+      const res = await axios.post(
+        "http://localhost:4000/user/register",
+        registerForm
+      );
       setMessage(res.data);
     } catch (err) {
       setMessage(err.response?.data || "Error registering");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setMessage("");
+
+    await new Promise((res) => setTimeout(res, 3000));
+
+    try {
+      const res = await axios.post("http://localhost:4000/login", loginForm);
+      setMessage(res.data);
+    } catch (err) {
+      setMessage(err.response?.data || "Error login");
     }
   };
 
@@ -23,7 +57,7 @@ function App() {
     <div style={{ padding: "2rem", fontFamily: "Arial" }}>
       <h2>Register</h2>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleRegister}
         style={{
           display: "flex",
           flexDirection: "column",
@@ -35,34 +69,36 @@ function App() {
           type="text"
           name="name"
           placeholder="Full Name"
-          value={form.name}
-          onChange={handleChange}
+          value={registerForm.name}
+          onChange={registerChange}
           required
         />
         <input
           type="password"
           name="pw"
           placeholder="Password"
-          value={form.pw}
-          onChange={handleChange}
+          value={registerForm.pw}
+          onChange={registerChange}
           required
         />
         <input
           type="text"
           name="username"
           placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
+          value={registerForm.username}
+          onChange={registerChange}
           required
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={loading}>
+          {loading ? "Please wait..." : "Register"}
+        </button>
       </form>
 
       {message && <p style={{ marginTop: "1rem" }}>{message}</p>}
 
       <h2>Log In</h2>
       <form
-        onSubmit={handleSubmit}
+        onSubmit={handleLogin}
         style={{
           display: "flex",
           flexDirection: "column",
@@ -74,19 +110,21 @@ function App() {
           type="text"
           name="username"
           placeholder="Username"
-          value={form.username}
-          onChange={handleChange}
+          value={loginForm.username}
+          onChange={loginChange}
           required
         />
         <input
           type="password"
           name="pw"
           placeholder="Password"
-          value={form.pw}
-          onChange={handleChange}
+          value={loginForm.pw}
+          onChange={loginChange}
           required
         />
-        <button type="submit">Register</button>
+        <button type="submit" disabled={loading}>
+          {loading ? "Please wait..." : "Login"}
+        </button>
       </form>
     </div>
   );
